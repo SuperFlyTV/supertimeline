6f22c159dd0b4cd38d8d54df42acedb4c3ad98fa
Julian Waller <git@julusian.co.uk>
2021-02-04 11:08:13 +0000

    feat: use @sofie-automation/code-standard-preset

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,23 @@
 module.exports = {
 	globals: {
 		'ts-jest': {
-			tsConfig: 'tsconfig.json'
-		}
+			tsconfig: 'tsconfig.json',
+		},
 	},
-	moduleFileExtensions: [
-		'ts',
-		'js'
-	],
+	moduleFileExtensions: ['ts', 'js'],
 	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest'
+		'^.+\\.(ts|tsx)$': 'ts-jest',
 	},
-	testMatch: [
-		'**/__tests__/**/*.spec.(ts|js)'
-	],
+	testMatch: ['**/__tests__/**/*.spec.(ts|js)'],
 	testEnvironment: 'node',
 	coverageThreshold: {
 		global: {
-		  branches: 0,
-		  functions: 0,
-		  lines: 0,
-		  statements: 0
-		}
+			branches: 0,
+			functions: 0,
+			lines: 0,
+			statements: 0,
+		},
 	},
-	coverageDirectory: "./coverage/",
-	collectCoverage: false
+	coverageDirectory: './coverage/',
+	collectCoverage: false,
 }
