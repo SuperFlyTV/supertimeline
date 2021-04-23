620fdd6a6448fca5e4825ca8102a6eb73053c1b1
Johan Nyman <johan@nytamin.se>
2021-04-23 20:20:18 +0200

    fix: bug when using the cache, and only changing the content of a timeline-object.

--- src/resolver/cache.ts
+++ src/resolver/cache.ts
@@ -10,16 +10,26 @@ export function initializeCache(cacheOrg: ResolverCache, resolvedTimeline: Resol
 
 	return cache
 }
-export function objectHash(obj: ResolvedTimelineObject): string {
+/** Return a "hash-string" which changes whenever anything that affects timing of a timeline-object has changed. */
+export function hashTimelineObject(obj: ResolvedTimelineObject): string {
 	const thingsThatMatter: string[] = [
 		JSON.stringify(obj.enable),
 		obj.disabled + '',
-		// obj.classes && obj.classes.join('_') || '',
 		obj.priority + '',
 		obj.resolved.parentId || '',
 		obj.resolved.isKeyframe + '',
 		obj.classes ? obj.classes.join('.') : '',
 		obj.layer + '',
+		obj.seamless + '',
+
+		/*
+		Note: The following properties are ignored, as they don't affect timing or resolving:
+		 * id
+		 * children
+		 * keyframes
+		 * isGroup
+		 * content
+		 */
 	]
 	return thingsThatMatter.join(',')
 }
